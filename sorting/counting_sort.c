596ab26c3d7cafdc0ba9e0fd526a275b63af546d
insuIoT <insoosos1234@gmail.com>
2021-09-09 11:54:13 +0900

    Initial commit

--- sorting/counting_sort.c
+++ sorting/counting_sort.c
@@ -21,21 +21,16 @@ int find_max(int data[], int size)
 
 void countingSort(int data[], int sort_data[], int size, int max)
 {
-    int count[max];
-    int i;
+    int count[max+1];
+    int i, j;
     for (i = 0; i <= max; i++)
         count[i] = 0;
-    print_sort("count[] 초기화", count, max);
-    for (int j = 0; j < size; j++)
-        count[data[j]] = count[data[j]] + 1;
-    print_sort("count[data[j]] 초기화", count, max);
-
-    for (int i = 0; i <= max; i++)
+    for (j = 0; j < size; j++) 
+        count[data[j]]++;
+    for (i = 1; i <= max; i++)
         count[i] = count[i] + count[i - 1];
-    print_sort("count[]: 정렬 ", count, max);
-
-    for (int j = size; j > 0; j--) {
-        sort_data[count[data[j]]] = data[j];
+    for (int j = size-1; j >= 0; j--) {
+        sort_data[count[data[j]]-1] = data[j];
         count[data[j]]--;
     }
 }
@@ -43,10 +38,10 @@ void countingSort(int data[], int sort_data[], int size, int max)
 int main()
 {
     int data[15] = { 1, 3, 2, 4, 1, 23, 12, 9, 18, 12, 3, 12, 3, 23, 23 };
-    int max = find_max(data, 15);
     int sort_data[15];
+    int max = find_max(data, 15);
     print_sort("Data: ", data, 15);
 
     countingSort(data, sort_data, 15, max);
-    print_sort("counting sort: ", data, 15);
+    print_sort("counting sort ", sort_data, 15);
 }
