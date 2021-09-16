c1cdc24fe111c57a3b4cb9c91efea1f152b59601
insuIoT <insoosos1234@gmail.com>
2021-09-16 16:26:52 +0900

    Initial commit

---
+++ graph/BFS_adjMatrix.c
@@ -0,0 +1,119 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <memory.h>
+
+#define MAX_VERTEX_NUM 100
+#define MAX_QUEUE_SIZE 20
+
+typedef struct {
+    int vertexNum;
+    int adjMatrix[MAX_VERTEX_NUM][MAX_VERTEX_NUM];
+} Graph;
+int visited[MAX_VERTEX_NUM];
+
+typedef int element;
+typedef struct {
+    int front;
+    int rear;
+    element data[MAX_QUEUE_SIZE];
+} QueueType;
+
+void error(char *message) {
+    fprintf(stderr, "%s\n", message);
+}
+
+void init_queue(QueueType *q) {
+    q->front = 0;
+    q->rear = 0;
+}
+
+int is_full(QueueType *q) {
+    return (q->front == (q->rear + 1) % MAX_QUEUE_SIZE);
+}
+
+int is_empty(QueueType *q) {
+    return (q->front == q->rear);
+}
+
+void enqueue(QueueType *q, element item) {
+    if(is_full(q)) {
+        error("Queue is full.");
+        return ;
+    }
+    q->rear = (q->rear + 1) % MAX_QUEUE_SIZE;
+    q->data[q->rear] = item;
+}
+
+element dequeue(QueueType *q) {
+    if(is_empty(q)) {
+        error("Queue is empty.");
+        return -1;
+    }
+    q->front = (q->front + 1) % MAX_QUEUE_SIZE;
+    return q->data[q->front];
+}
+
+Graph* NewGraph(int vNum) {
+    Graph* graph = (Graph*)malloc(sizeof(Graph));
+    graph->vertexNum = vNum;
+    //Reset: graph->adjMatrix  
+    memset(graph->adjMatrix, 0, sizeof(int)*MAX_VERTEX_NUM*MAX_VERTEX_NUM);
+
+    return graph;
+}
+
+void AddEdge(Graph *graph, int start, int end) {
+    graph->adjMatrix[start][end] = 1;
+    graph->adjMatrix[end][start] = 1;
+}
+
+void printGraph(Graph *graph) {
+    for(int i=1; i<=graph->vertexNum; i++) {
+        for(int j=1; j<=graph->vertexNum; j++)
+            printf("%d ", graph->adjMatrix[i][j]);
+
+        printf("\n");
+    }
+}
+
+void BFS_Mat(Graph *g, int s) {
+    QueueType q;
+    init_queue(&q);
+
+    for(int i=1; i<=g->vertexNum; i++)
+        visited[i] = 0;
+
+    visited[s] = 1;
+    printf("%d ", s);
+    enqueue(&q, s);
+    while(!is_empty(&q)) {
+        int u = dequeue(&q);
+        for(int v=1; v<=g->vertexNum; v++) {
+            if(g->adjMatrix[u][v] == 1 && visited[v] == 0) {
+                visited[v] = 1;
+                printf("%d ", v);
+                enqueue(&q, v);
+            }
+        }
+    }
+}
+
+int main() {
+    Graph *g = NewGraph(8);
+
+    AddEdge(g, 1, 2);
+    AddEdge(g, 1, 3);
+    AddEdge(g, 2, 4);
+    AddEdge(g, 2, 5);
+    AddEdge(g, 3, 5);
+    AddEdge(g, 3, 7);
+    AddEdge(g, 3, 8);
+    AddEdge(g, 4, 5);
+    AddEdge(g, 5, 6);
+    AddEdge(g, 7, 8);
+    
+
+    printGraph(g);
+    printf("\nBFS: ");
+    BFS_Mat(g, 1);
+}
