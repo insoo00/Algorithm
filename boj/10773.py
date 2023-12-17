b376beeb95da155354d2a278fd3984855629f608
insoo00 <insoosos1234@gmail.com>
2023-12-17 21:56:39 +0900

    first commit

---
+++ boj/10773.py
@@ -0,0 +1,46 @@
+n = int(input())
+result = []
+
+for _ in range(n):
+    result.append(int(input()))
+
+# 병합 정렬(Merge sort)
+def merge_sort(arr):
+    def sort(low, high):
+        if high - low < 2:
+            return
+        mid = (low + high) // 2
+        sort(low, mid)
+        sort(mid, high)
+        merge(low, mid, high)
+
+    def merge(low, mid, high):
+        temp = []
+        l, h = low, mid
+
+        while l < mid and h < high:
+            if arr[l] < arr[h]:
+                temp.append(arr[l])
+                l += 1
+            else:
+                temp.append(arr[h])
+                h += 1
+
+        while l < mid:
+            temp.append(arr[l])
+            l += 1
+        while h < high:
+            temp.append(arr[h])
+            h += 1
+
+        for i in range(low, high):
+            arr[i] = temp[i - low]
+
+    return sort(0, len(arr))
+
+for i in merge_sort(result):
+    print(i)
+
+
+# 퀵 정렬(Quick sort)
+# 힙 정렬(Heap sort)
