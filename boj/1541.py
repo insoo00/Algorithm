3b4b685dee1468c506c00ca2aee023b994ae226f
insoo00 <insoosos1234@gmail.com>
2024-03-05 10:25:32 +0900

    greedy

--- boj/1541.py
+++ boj/1541.py
@@ -1,3 +1,4 @@
+#1
 import sys
 input = lambda: sys.stdin.readline().rstrip()
 
@@ -15,3 +16,44 @@ for num_eval in nums_eval[1:]:
     result -= num_eval
 
 print(result)
+
+#2
+expression = input()
+expression_list = []
+
+number = ''
+for idx, item in enumerate(expression):
+    if item == '+':
+        if number != '':
+            expression_list.append(int(number))
+            number = ''
+        expression_list.append(item)
+    elif item == '-':
+        if number != '':
+            expression_list.append(int(number))
+            number = ''
+        expression_list.append(item)
+    else:
+        number += item
+        if idx == len(expression)-1:
+            expression_list.append(int(number))
+
+result = ''
+has_minus = False
+for idx, item in enumerate(expression_list):
+    if item == '-':
+        if has_minus == False:
+            result += '-('
+            has_minus = True
+        else:
+            result += ')-('
+    elif item == '+':
+        result += item
+    else:
+        result += str(item)
+    
+    if idx == len(expression_list)-1:
+        if has_minus == True:
+            result += ')'
+
+print(eval(result))
