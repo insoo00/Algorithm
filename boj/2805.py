1af94fc4adb0c9375a657fc139acfd0610b342a0
insoo00 <insoosos1234@gmail.com>
2024-04-15 13:48:26 +0900

    binary search

--- boj/2805.py
+++ boj/2805.py
@@ -1,3 +1,4 @@
+#1
 import sys
 input = lambda: sys.stdin.readline().rstrip()
 
@@ -13,6 +14,28 @@ while start <= end:
         if tree>mid:
             sum += tree-mid
         
+    if sum >= M:
+        start = mid+1
+    else:
+        end = mid-1
+
+print(end)
+
+#2
+N, M = map(int, input().split())
+trees = list(map(int, input().split()))
+start, end = 0, max(trees)
+
+while start <= end:
+    mid = (start+end)//2
+    # print(f'start: {start}, end: {end}, mid: {mid}')
+
+    sum = 0
+    for tree in trees:
+        if tree>mid:
+            sum += tree-mid
+
+    # print(f'sum: {sum}')
     if sum >= M:
         start = mid+1
     else:
