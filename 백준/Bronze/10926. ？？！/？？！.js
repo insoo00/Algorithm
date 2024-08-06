5f395bd0e81a130cd1dd0dd4ef75a0e090431b66
Insoo Lee <77774140+insoo00@users.noreply.github.com>
2024-08-07 01:18:59 +0900

    [Bronze V] Title: ??!, Time: 92 ms, Memory: 9332 KB -BaekjoonHub

--- 백준/Bronze/10926. ？？！/？？！.js
+++ 백준/Bronze/10926. ？？！/？？！.js
@@ -1,6 +1,4 @@
 let fs = require("fs");
-// let input = fs.readFileSync("input.txt").toString().split(" ");
 let input = fs.readFileSync("/dev/stdin").toString().trim();
-// let input = fs.readFileSync().toString().split(" ")
 
 console.log(input + "??!");
