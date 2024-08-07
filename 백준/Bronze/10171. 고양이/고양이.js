12803f2222a017fdc3cc561f3efdd519340bc00c
Insoo Lee <77774140+insoo00@users.noreply.github.com>
2024-08-07 22:52:52 +0900

    [Bronze V] Title: 고양이, Time: 116 ms, Memory: 9328 KB -BaekjoonHub

---
+++ 백준/Bronze/10171. 고양이/고양이.js
@@ -0,0 +1,12 @@
+let fs = require("fs");
+// let input = fs.readFileSync("input.txt").toString().split(" ");
+// let input = fs.readFileSync("/dev/stdin").toString().split(" ")
+
+let answer = "";
+
+answer += "\\    /\\" + "\n";
+answer += " )  ( ')" + "\n";
+answer += "(  /  )" + "\n";
+answer += " \\(__)|" + "\n";
+
+console.log(answer);
