515e458bb13d39d262c3afe63f6fb3d33ae88970
Insoo Lee <77774140+insoo00@users.noreply.github.com>
2024-08-07 23:02:25 +0900

    [Bronze V] Title: 개, Time: 88 ms, Memory: 9316 KB -BaekjoonHub

---
+++ 백준/Bronze/10172. 개/개.js
@@ -0,0 +1,13 @@
+let fs = require("fs");
+// let input = fs.readFileSync("input.txt").toString().split(" ");
+// let input = fs.readFileSync("/dev/stdin").toString().split(" ")
+
+let answer = "";
+
+answer += `|\\_/|` + "\n";
+answer += `|q p|   /}` + "\n";
+answer += `( 0 )"""\\` + "\n";
+answer += `|"^"\`    |` + "\n";
+answer += `||_/=\\\\__|`;
+
+console.log(answer);
