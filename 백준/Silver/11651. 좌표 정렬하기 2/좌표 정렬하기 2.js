c0bfa312125ac3ade334c31f7ba9e9d86393a976
Insoo Lee <77774140+insoo00@users.noreply.github.com>
2024-08-10 14:01:18 +0900

    [Silver V] Title: 좌표 정렬하기 2, Time: 3696 ms, Memory: 62700 KB -BaekjoonHub

---
+++ 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.js
@@ -0,0 +1,26 @@
+let fs = require("fs");
+// let input = fs.readFileSync("input.txt").toString().split("\n");
+let input = fs.readFileSync("/dev/stdin").toString().split("\n");
+
+const n = Number(input[0]);
+let arr = [];
+
+for (let i=1; i<n+1; i++) {
+  const current = input[i].split(" ").map(Number);
+  arr.push(current);
+  
+}
+
+arr.sort((a, b) => {
+  if (a[1] != b[1]) {
+    return a[1] - b[1];
+  } else {
+    return a[0] - b[0];
+  }
+});
+
+// console.log(arr);
+
+for (let i=0; i<n; i++) {
+  console.log(arr[i][0] + " " + arr[i][1]);
+}
