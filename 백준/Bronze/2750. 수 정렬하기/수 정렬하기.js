cb99e708b7c1d1cb326576d43b0acfd8911c31f8
Insoo Lee <77774140+insoo00@users.noreply.github.com>
2024-08-12 01:47:57 +0900

    [Bronze II] Title: 수 정렬하기, Time: 160 ms, Memory: 9984 KB -BaekjoonHub

--- 백준/Bronze/2750. 수 정렬하기/수 정렬하기.js
+++ 백준/Bronze/2750. 수 정렬하기/수 정렬하기.js
@@ -1,25 +1,46 @@
+function selectionSort(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    let minIndex = i;
+    for (let j = i; j < arr.length; j++) {
+      if (arr[minIndex] > arr[j]) {
+        minIndex = j;
+      }
+    }
+    [arr[minIndex], arr[i]] = [arr[i], arr[minIndex]];
+  }
+}
+
+function bubbleSort(arr) {
+  for (let i=arr.length-1; i>0; i--) {
+    for (let j=0; j<i; j++) {
+      if (arr[j] > arr[j+1]) {
+        [arr[j], arr[j+1]] = [arr[j+1], arr[j]];
+      }
+    }
+  }
+}
+
+function insertionSort(arr) {
+  for (let i=1; i<arr.length; i++) {
+    for (let j=i; j>0; j--) {
+      if (arr[j-1] > arr[j]) {
+        [arr[j-1], arr[j]] = [arr[j], arr[j-1]];
+      } else {
+        break;
+      }
+    }
+  }
+}
+
 let fs = require("fs");
 // let input = fs.readFileSync("input.txt").toString().split("\n");
 let input = fs.readFileSync("/dev/stdin").toString().split("\n");
 
-const n = Number(input[0])
-let arr = []
-
-for (let i=1; i<n+1; i++) {
-  arr.push(Number(input[i]))
+const n = Number(input[0]);
+let nums = [];
+for (let i = 1; i < n+1; i++) {
+  nums.push(Number(input[i]));
 }
 
-selectionSort(arr);
-console.log(arr.join("\n"));
-
-function selectionSort(arr) {
-  for (let i=0; i<arr.length; i++) {
-    let minIndex = i;
-    for (let j=i+1; j<arr.length; j++) {
-      if(arr[minIndex]>arr[j]) {
-        minIndex = j;
-      }
-    }
-    [arr[minIndex], arr[i]] = [arr[i], arr[minIndex]]
-  }
-}
+insertionSort(nums);
+console.log(nums.join("\n"));
