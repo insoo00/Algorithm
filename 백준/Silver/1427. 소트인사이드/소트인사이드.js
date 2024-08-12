e21959f56916fe5ab3a03e48ef36453bbab113a6
Insoo Lee <77774140+insoo00@users.noreply.github.com>
2024-08-12 22:37:04 +0900

    [Silver V] Title: 소트인사이드, Time: 92 ms, Memory: 9320 KB -BaekjoonHub

--- 백준/Silver/1427. 소트인사이드/소트인사이드.js
+++ 백준/Silver/1427. 소트인사이드/소트인사이드.js
@@ -2,24 +2,11 @@ let fs = require("fs");
 // let input = fs.readFileSync("input.txt").toString().trim();
 let input = fs.readFileSync("/dev/stdin").toString().trim();
 
-// let arr = [];
-// for (let i=0; i<input.length; i++) {
-//   arr.push(Number(input[i]));
-// }
-
-let arr = input.split('');
-
+let arr = [];
+for (let i=0; i<input.length; i++) {
+  arr.push(Number(input[i]));
+}
 
 arr.sort((a,b) => b-a);
 
-
 console.log(arr.join(""))
-
-// // input값 처리
-// const arr = require('fs').readFileSync('/dev/stdin').toString().trim().split('');
-
-
-
-// sorted = arr.sort((a,b) => b - a);  // 내림차순 정렬
-
-// sorted.join('');
