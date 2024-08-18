2ea36acf5cc9a6704e53096c2b62f40c74fb458b
Insoo Lee <77774140+insoo00@users.noreply.github.com>
2024-08-18 13:52:59 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 1072 ms, Memory: 199908 KB -BaekjoonHub

--- 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.js
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.js
@@ -1,44 +1,49 @@
-function merge(arr, left, mid, right) {
-  let i = left;  //왼쪽 배열 인덱스
-  let j = mid+1; //오른쪽 배열 인덱스
-  let k = left;  //병합 배열 인덱스
-  while (i<=mid && j<=right) {
-    if (arr[i] <= arr[j]) sorted[k++] = arr[i++];
-    else sorted[k++] = arr[j++];
-  }
-  if (i > mid) {
-    while (j <= right) sorted[k++] = arr[j++];
-  } else {
-    while (i <= mid) sorted[k++] = arr[i++];
-  }
-
-  for (let x=left; x<=right; x++) arr[x] = sorted[x]
-}
-
-function mergeSort(arr, left, right) {
-  if (left < right) {
-    let mid = parseInt((left+right)/2);
-    mergeSort(arr, left, mid);
-    mergeSort(arr, mid+1, right);
-    merge(arr, left, mid, right);
-  }  
+function quickSort(arr, left, right) {
+    if (left >= right) {
+        // 원소가 2개 이상일 때만 수행
+        return;
+    }
+    // [3, 5, 6, 8, 9]
+    //        l     r
+    // right - left + 1 => 3이 나옴
+    //
+    let pivot = Math.floor(Math.random() * (right - left + 1)); // [0, 원소 개수)에서 추출
+    // 0, 1, 2 중에 하나 고르게 됨.
+    pivot += left; // 2, 3, 4 중에 하나 고르게 됨
+    // let pivot = right;
+    [arr[pivot], arr[left]] = [arr[left], arr[pivot]];
+    pivot = left;
+    let i = left + 1;
+    let j = right;
+    while (i <= j) {
+        // 왼쪽에서는 pivot보다 큰 데이터를 찾음
+        while (i <= right && arr[i] <= arr[pivot]) i++;
+        // 오른쪽에서는 pivot보다 작은 데이터를 찾음
+        while (j > left && arr[j] >= arr[pivot]) j--;
+        // 엇갈렸다면 pivot이랑 j이랑 교체
+        if (i > j) [arr[pivot], arr[j]] = [arr[j], arr[pivot]];
+        // 엇갈리지 않았다면 i와 j를 교체
+        else [arr[i], arr[j]] = [arr[j], arr[i]];
+    }
+    // 여기까지 오면, pivot을 기준으로 작은 데이터는 모두 왼쪽에, 큰 건 모두 오른
+    // 왼쪽과 오른쪽을 기준으로 각각 마찬가지로 정렬 수행
+    quickSort(arr, left, j - 1);
+    quickSort(arr, j + 1, right);
 }
 
 let fs = require("fs");
-// let input = fs.readFileSync("input.txt").toString().split("\n");
 let input = fs.readFileSync("/dev/stdin").toString().split("\n");
 
-const n = Number(input[0]);
-let nums = [];
-for (let i = 1; i < n+1; i++) {
-  nums.push(Number(input[i]));
+let n = Number(input[0]);
+let arr = [];
+for (let i = 1; i <= n; i++) {
+    arr.push(Number(input[i]));
 }
-let sorted = Array.from({length: n}, () => -1)
 
-mergeSort(nums, 0, n-1);
+quickSort(arr, 0, arr.length - 1);
+
 let answer = "";
-for (num of nums) {
-  answer += num + "\n";
+for (let i = 0; i < arr.length; i++) {
+    answer += arr[i] + "\n";
 }
-
-console.log(answer)
+console.log(answer);
