0cebb08be1a1f763f94d37f3718ed2eafac4c400
Insoo Lee <77774140+insoo00@users.noreply.github.com>
2024-08-19 21:20:05 +0900

    [Silver I] Title: 숨바꼭질, Time: 232 ms, Memory: 17284 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.js
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.js
@@ -26,7 +26,7 @@ let fs = require("fs");
 let input = fs.readFileSync("/dev/stdin").toString().split("\n");
 
 const [n, k] = input[0].split(" ").map(Number);
-let field = Array.from({ length: 200000 }, () => -1);
+let field = Array.from({ length: 100001 }, () => -1);
 
 let q = new Queue();
 q.enqueue(n);
@@ -40,7 +40,7 @@ while (field[k] == -1) {
   nexts.push(cur * 2);
 
   for (next of nexts) {
-    if (next < 0 || next > 200000) continue;
+    if (next < 0 || next > 100001) continue;
     if (field[next] == -1) {
       q.enqueue(next);
       field[next] = field[cur] + 1;
