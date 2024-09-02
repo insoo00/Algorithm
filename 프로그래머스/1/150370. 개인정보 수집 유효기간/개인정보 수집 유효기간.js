2f652f18ee70d358414dc16c2ac5fd32abebd036
Insoo Lee <77774140+insoo00@users.noreply.github.com>
2024-09-02 16:30:03 +0900

    [level 1] Title: 개인정보 수집 유효기간, Time: 0.98 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.js
+++ 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.js
@@ -13,25 +13,13 @@ function solution(today, terms, privacies) {
         let [date, t] = privacy.split(" ");
         let [dateY, dateM, dateD] = date.split(".").map(Number);
         
-        // dateM += termsInfo[t];
-        // if (dateM > 12) {
-        //     dateY += 1;
-        //     dateM -= 12;
-        // }
         
-        const checkDate=(year, month, exp)=>{
-            const testMonth=exp+month;
-            let newYear=testMonth%12===0 ? year+(parseInt(testMonth/12))-1 : year+(parseInt(testMonth/12))
-            let newMonth=testMonth%12===0 ? 12 : testMonth%12
-
-            return {'year': newYear,'month':newMonth}
-        }
-        
-        if(dateM+termsInfo[t]>12) {
-            dateY = checkDate(dateY, dateM, termsInfo[t]).year
-            dateM = checkDate(dateY, dateM, termsInfo[t]).month
+        let tmpM = dateM+termsInfo[t];
+        if(tmpM>12) {
+            dateY = tmpM%12===0 ? dateY+(parseInt(tmpM/12))-1 : dateY+(parseInt(tmpM/12))
+            dateM = tmpM%12===0 ? 12 : tmpM%12
         } else {
-            dateM = dateM+termsInfo[t]
+            dateM = tmpM
         }
 
 
