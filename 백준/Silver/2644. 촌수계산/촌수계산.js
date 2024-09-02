42b3107c7193e47393a40fe8fac40f4a053b4831
Insoo Lee <77774140+insoo00@users.noreply.github.com>
2024-09-02 19:51:21 +0900

    [Silver II] Title: 촌수계산, Time: 88 ms, Memory: 9368 KB -BaekjoonHub

--- 백준/Silver/2644. 촌수계산/촌수계산.js
+++ 백준/Silver/2644. 촌수계산/촌수계산.js
@@ -1,32 +1,34 @@
-let input = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n');
-const n = Number(input.shift()); // 전체 사람 수
-const [a, b] = input.shift().split(' ').map(Number); // a, b 두 사람의 번호
-const m = Number(input.shift()); // 관계의 개수
-const arr = input.map((v) => v.split(' ').map(Number)); // 부모-자식 관계 배열
-
-let answer, degree = 0;
-let visited = Array(n + 1).fill(false);
-let graph = [...Array(n + 1)].map(() => []);
-
-// 양방향 그래프 만들기
-arr.map(([from, to]) => {
-  graph[from].push(to);
-  graph[to].push(from);
-});
-
-
-// DFS
-const dfs = (start, depth) => {
-  // start가 b에 도달하면 depth answer에 저장
-  if (start === b) answer = depth;
-
-    // 현재 노드 방문처리 안 되어있다면 방문 처리하고 dfs실행하는데 depth는 1씩 증가
-  for (const v of graph[start]) {
-      if (!visited[v]) {
-        visited[v] = true;
-        dfs(v, depth + 1);
-      }
+function dfs(x, y, depth) {
+  visited[x] = true;
+  // console.log(`x: ${x} y: ${y} depth: ${depth}, visited: ${visited}`);
+  if (x == y) {
+    answer = depth;
+    return;
   }
-};
-dfs(a, degree);
-answer ? console.log(answer) : console.log(-1);
+
+  for (let g of graph[x]) {
+    if (!visited[g]) {
+      visited[g] = true;
+      dfs(g, y, depth + 1);
+    }
+  }
+}
+
+const fs = require("fs");
+const input = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
+
+const n = Number(input[0]);
+const [x, y] = input[1].split(" ").map(Number);
+const m = Number(input[2]);
+let graph = Array.from({ length: n + 1 }, () => []);
+let visited = Array.from({ length: n + 1 }, () => false);
+for (let i = 3; i < m + 3; i++) {
+  const [parent, child] = input[i].split(" ").map(Number);
+  graph[parent].push(child);
+  graph[child].push(parent);
+}
+
+let answer = -1;
+dfs(x, y, 0);
+
+console.log(answer);
