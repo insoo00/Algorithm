1b6183142f9bbd562ee64cf093c3ca9c3cb95980
Insoo Lee <77774140+insoo00@users.noreply.github.com>
2024-09-07 15:23:27 +0900

    [level 4] Title: FrontEnd 개발자 찾기, Time: , Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/4/276035. FrontEnd 개발자 찾기/FrontEnd 개발자 찾기.sql
@@ -0,0 +1,15 @@
+with SKILLCODES_TMP as (
+    select CATEGORY, BIN(CODE) as CODE
+    from SKILLCODES
+),
+DEVELOPERS_TMP as (
+    select id, first_name, last_name, email, BIN(SKILL_CODE) as SKILL_CODE
+    from DEVELOPERS
+)
+
+select distinct a.id, a.email, a.first_name, a.last_name
+from DEVELOPERS_TMP as a 
+left join SKILLCODES_TMP as b
+on ((a.skill_code - b.code) >= 0) and ((a.skill_code - b.code) not like '%9%')
+where b.category = 'Front End'
+order by a.id;
