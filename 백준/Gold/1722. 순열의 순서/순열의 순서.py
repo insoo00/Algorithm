84b2da0540ce56e0025df72b3666e7a8c1546136
Insoo Lee <77774140+insoo00@users.noreply.github.com>
2024-10-01 10:14:44 +0900

    [Gold V] Title: 순열의 순서, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/1722. 순열의 순서/순열의 순서.py
+++ 백준/Gold/1722. 순열의 순서/순열의 순서.py
@@ -5,23 +5,23 @@ input = sys.stdin.readline
 n = int(input())
 inputs = input().split()
 
-dp = [1] * (n + 1)
+factorial = [1] * (n + 1)
 for i in range(2, n + 1):
-    dp[i] = dp[i - 1] * i
+    factorial[i] = factorial[i - 1] * i
 
 if inputs[0] == '1':
     idx = int(inputs[1])
-    nums = [i for i in range(1, n+1)]
+    nums = set([i for i in range(1, n+1)])
     res = []
     offset = 0
 
-    while len(res) != n: 
+    for length in range(n):
         index = 0
         for num in nums:
             index += 1
-            if idx <= offset + index * dp[len(nums)-1]:
+            if idx <= offset + index * factorial[n-length-1]:
+                offset += (index -1) * factorial[n-length-1]
                 res.append(num)
-                offset += (index -1) * dp[len(nums)-1]
                 nums.remove(num)
                 break
     print(*res)
@@ -38,7 +38,7 @@ else:
                 cnt += 1
             else:
                 break
-        offset += cnt * dp[len(nums)-1] 
+        offset += cnt * factorial[len(nums)-1] 
         nums.remove(d)
 
     print(offset)
