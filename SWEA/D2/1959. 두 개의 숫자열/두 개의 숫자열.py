c6501fc2fca0a8bf4d55075f18cb5991c049d969
Insoo Lee <77774140+insoo00@users.noreply.github.com>
2024-10-08 13:01:44 +0900

    [D2] Title: 두 개의 숫자열, Time: 106 ms, Memory: 47,464 KB -BaekjoonHub

--- SWEA/D2/1959. 두 개의 숫자열/두 개의 숫자열.py
+++ SWEA/D2/1959. 두 개의 숫자열/두 개의 숫자열.py
@@ -1,22 +1,22 @@
 # import sys
 # sys.stdin = open("input.txt", "r")
-
+ 
 T = int(input())
 for test_case in range(1, T + 1):
     n, m = map(int, input().split())
     a = list(map(int, input().split()))
     b = list(map(int, input().split()))
-    
+     
     big = list()
     small = list()
-    
+     
     if len(a) > len(b):
         big = a
         small = b
     else: 
         big = b
         small = a
-
+ 
     cnt = len(big)-len(small)+1
     result = -1e9
     small += [0]*(len(big)-len(small))
@@ -26,15 +26,10 @@ for test_case in range(1, T + 1):
             tmp += big[i]*small[i]
         # print(f'big: {big}, small: {small}, tmp: {tmp}')
         result = max(result, tmp)
+        small_last = small[-1]
         for j in range(len(small)-1, 0, -1):
             small[j] = small[j-1]
-        small[0] = 0
-
+        small[0] = small_last
+ 
     print(f'#{test_case} {result}')
-            
-
-
-
-
-
-    
+             
