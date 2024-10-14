866726154556386f4c11648fcc711db125ccc55d
Insoo Lee <77774140+insoo00@users.noreply.github.com>
2024-10-14 13:20:45 +0900

    [Gold V] Title: 로봇 청소기, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Gold/14503. 로봇 청소기/로봇 청소기.py
@@ -0,0 +1,37 @@
+n,m = map(int,input().split())
+graph = []
+visited = [[False] * m for _ in range(n)]
+r,c,d = map(int,input().split())
+
+# 북(0) -> 동(1) -> 남(2) -> 서(3)
+dx = [-1,0,1,0]
+dy = [0,1,0,-1]
+
+for _ in range(n):
+    graph.append(list(map(int,input().split())))
+
+visited[r][c] = True
+cnt = 1
+
+while True:
+    cleanup = False
+    for _ in range(4):
+        nx = r + dx[(d+3)%4]
+        ny = c + dy[(d+3)%4]
+        d = (d+3)%4
+        if nx<0 or nx>=n or ny<0 or ny>=m:
+            continue
+        if graph[nx][ny] == 0:
+            if not visited[nx][ny]:
+                visited[nx][ny] = True
+                cnt += 1
+                r = nx
+                c = ny
+                cleanup = True
+                break
+    if not cleanup:
+        if graph[r-dx[d]][c-dy[d]] == 1:
+            print(cnt)
+            break
+        else:
+            r,c = r-dx[d],c-dy[d]
