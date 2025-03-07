5ce5def3edcab09db45a92a5b4290bd2e39b4ce2
Insoo Lee <77774140+insoo00@users.noreply.github.com>
2025-03-07 20:40:17 +0900

    [Gold IV] Title: 공유기 설치, Time: 5224 ms, Memory: 40140 KB -BaekjoonHub

--- 백준/Gold/2110. 공유기 설치/공유기 설치.py
+++ 백준/Gold/2110. 공유기 설치/공유기 설치.py
@@ -9,24 +9,19 @@ end = houses[-1]-houses[0]
 result = -1
 
 while start <= end:
-    mid = (start+end)//2 # 두 공유기 사이의 거리
-    # print(f'mid: {mid}')
+    mid = (start+end)//2            # 두 공유기 사이의 거리
 
-    cnt = 1 # 공유기 설치 대수 
-    cur = houses[0] # 현재 공유기 설치한 집
-    for house in houses: # 다음 공유기 설치 가능한 집 찾기
+    cnt = 1                         # 공유기 설치 대수 (첫 번째 집 설치 시작)
+    cur = houses[0]                 # 현재 공유기 설치한 집
+    for house in houses:            # 다음 공유기 설치 가능한 집 찾기
         if house - cur >= mid:
             cnt += 1
             cur = house
-            # print(f'house: {house}')
         
-    if cnt >= C: # 공유기 다 설치함 -> 거리를 벌려야됨
+    if cnt >= C:                    # 공유기 다 설치함 -> 거리를 벌려야됨
         result = mid
         start = mid+1
     else:
         end = mid-1
 
 print(result)
-
-
-
