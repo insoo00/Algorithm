4fe2d41bd3bdc5d1e0c2d2279f100940b85f1794
Insoo Lee <77774140+insoo00@users.noreply.github.com>
2025-03-08 01:25:04 +0900

    [Gold IV] Title: 이분 그래프, Time: 1116 ms, Memory: 54000 KB -BaekjoonHub

--- 백준/Gold/1707. 이분 그래프/이분 그래프.py
+++ 백준/Gold/1707. 이분 그래프/이분 그래프.py
@@ -1,44 +1,42 @@
-from collections import deque
 import sys
+from collections import deque
+
 input = sys.stdin.readline
 
+def bfs(i):
+    q= deque()
+    q.append(i)
+    visited[i] = 0
 
-def BFS(start, group):
-    que = deque([start])
-    visited[start] = group
+    while q:
+        cur = q.popleft()
 
-    while que:
-        node = que.popleft()
+        for next in graph[cur]:
+            if visited[next] == -1:
+                visited[next] = (visited[cur]+1)%2
+                q.append(next)
+            elif visited[next] == visited[cur]:  # 같은 색이라면 이분 그래프가 아님
+                return False
+    return True
 
-        for link_node in graph[node]:
-            if not visited[link_node]:
-                que.append(link_node)
-                visited[link_node] = -visited[node]
-            elif visited[link_node] == visited[node]:
-                return True
+K = int(input())
+for _ in range(K):
+    V, E = map(int, input().split())
+    graph = [[] for _ in range(V+1)]
+    visited = [-1 for _ in range(V+1)]
+    for _ in range(E):
+        u,v = map(int, input().split())
+        graph[u].append(v)
+        graph[v].append(u)
 
-    return False
+    answer = True
+    for i in range(1, V+1):
+        if visited[i] == -1:
+            if not bfs(i):
+                answer = False
+                break
 
-
-if __name__ == "__main__":
-    K = int(input())
-
-    for _ in range(K):
-        V, E = map(int, input().split())
-
-        graph = [[] for _ in range(V+1)]
-        for _ in range(E):
-            u, v = map(int, input().split())
-            graph[u].append(v)
-            graph[v].append(u)
-
-        visited = [False] * (V+1)
-
-        for i in range(1, V+1):
-            if not visited[i]:
-                result = BFS(i, 1)
-                if result:
-                    print("NO")
-                    break
-        else:
-            print("YES")
+    if answer == True:
+        print('YES')
+    else:
+        print('NO')
